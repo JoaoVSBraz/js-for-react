c9b219548e8c0b6bbbe4403109eb7d090d6af50e
JoaoVSBraz <joaovictor3.14@outlook.com>
2021-07-08 15:52:00 -0300

    Some updates

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import "./src/styles/elements/base.css";
 import CardGame from "./src/components/CardGame";
 
 const $root = document.querySelector("#root");
+
 const $htmlCardGame = CardGame();
 
 $root.insertAdjacentHTML("beforeend", $htmlCardGame);
