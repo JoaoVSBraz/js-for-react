2bfb1062469551577dd34f837992b62f2c87fa7b
JoaoVSBraz <joaovictor3.14@outlook.com>
2021-07-23 11:44:10 -0300

    :lipstick: Update Style Files

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const $root = document.querySelector("#root");
 $root.insertAdjacentHTML(
     "beforeend", 
     `
-        ${PlayerName()}
-        ${PlayerName()}
+        ${PlayerName('Player1')}
+        ${PlayerName('Player2')}
         ${BoardGame(6)}
     `
 );
